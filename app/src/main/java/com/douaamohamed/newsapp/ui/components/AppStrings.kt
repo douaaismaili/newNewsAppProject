f47cca2ffcfb6fe6fd25405bb63d367f29bd2371
mohamedghacham <amranilara@gmail.com>
2026-01-17 23:52:41 +0100

    AppStrings Modified

--- app/src/main/java/com/douaamohamed/newsapp/ui/components/AppStrings.kt
+++ app/src/main/java/com/douaamohamed/newsapp/ui/components/AppStrings.kt
@@ -1,9 +1,12 @@
 package com.douaamohamed.newsapp.ui.components
 
 object AppStrings {
+    const val APP_NAME = "Actualités Pro"
     const val UNKNOWN = "Inconnu"
     const val SOMETHING_WENT_WRONG = "Une erreur est survenue"
     const val ARTICLE_SAVED_SUCCESSFULLY = "Article sauvegardé avec succès"
     const val ARTICLE_REMOVED_FROM_FAVORITES = "Article retiré des favoris"
-    // Ajoute d'autres si besoin
+    const val SHARE_VIA = "Partager via"
+    const val IMPOSSIBLE_SHARE = "Impossible de partager"
+    const val NO_INTERNET = "Pas de connexion internet"
 }
